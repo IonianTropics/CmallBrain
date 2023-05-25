f74b52c8ae10dea139afa139e6c1e7c471bb34c8
Leah Bowen <63286932+IonianTropic@users.noreply.github.com>
2023-05-25 12:58:24 -0600

    Update formatting in CmallBrain.c

--- src/CmallBrain.c
+++ src/CmallBrain.c
@@ -1,49 +1,71 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char* buffer;
 FILE* istream;
 FILE* ostream;
 char c;
 
-int main( int argc, char * argv[] )
-{
-	if (argc == 3)
-	{
-		istream = fopen(argv[1], "r");
-		if (istream)
-		{
-			ostream = fopen(argv[2], "w");
-			fputs("#include <stdio.h>\nchar array[30000] = {0}; char* ptr = array;\nint main() {\n\t", ostream);
-			while ((c = fgetc(istream)) != EOF)
-			{
-				switch (c)
-				{
-					case '>': fputs("++ptr; ", ostream); 		break;
-					case '<': fputs("--ptr; ", ostream); 		break;
-					case '+': fputs("++*ptr; ", ostream); 		break;
-					case '-': fputs("--*ptr; ", ostream); 		break;
-					case '.': fputs("putchar(*ptr); ", ostream); 	break;
-					case ',': fputs("*ptr = getchar(); ", ostream); break;
-					case '[': fputs("while (*ptr) { ", ostream);	break;
-					case ']': fputs(" }", ostream);			break;
-					default: 					break;
-				}
-			}
-      fclose(istream);
-			fputs("\n\treturn 0;\n}\n", ostream);
-			fclose(ostream);
-		}
-		else
-		{
-			fprintf(stderr, "Could not open file: %s", argv[1]);
-			exit(1);
-		}
-	}
-	else
-	{
-		fprintf(stderr, "Expected source and output arguments");
-		exit(1);
-	}
-	return 0;
+int main( int argc, char * argv[] ) {
+
+    if (argc != 3) {
+        fprintf(stderr, "Expected source and output arguments");
+        return 1;
+    }
+
+    istream = fopen(argv[1], "r");
+    if (!istream) {
+        fprintf(stderr, "Could not open file: %s", argv[1]);
+        return 1;
+    }
+
+    ostream = fopen(argv[2], "w");
+    if (!ostream) {
+        fprintf(stderr, "Could not open file: %s", argv[2]);
+        return 1;
+    }
+    
+    fputs("#include <stdio.h>\n", ostream);
+    fputs("char array[30000] = {0}; char* ptr = array;\n", ostream);
+    fputs("int main() {\n\t", ostream);
+
+    while ((c = fgetc(istream)) != EOF) {
+        process_char();
+    }
+
+    fputs("\n\treturn 0;\n}\n", ostream);
+    fclose(istream);
+    fclose(ostream);
+
+    return 0;
+}
+
+void process_char() {
+    switch (c) {
+        case '>':
+            fputs("++ptr; ", ostream);
+            break;
+        case '<':
+            fputs("--ptr; ", ostream);
+            break;
+        case '+':
+            fputs("++*ptr; ", ostream);
+            break;
+        case '-':
+            fputs("--*ptr; ", ostream);
+            break;
+        case '.':
+            fputs("putchar(*ptr); ", ostream);
+            break;
+        case ',':
+            fputs("*ptr = getchar(); ", ostream);
+            break;
+        case '[':
+            fputs("while (*ptr) { ", ostream);
+            break;
+        case ']':
+            fputs(" }", ostream);
+            break;
+        default:
+            break;
+    }
 }
